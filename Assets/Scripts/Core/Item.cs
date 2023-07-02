48412c00cdce9090ea80aedb1715c13afa93ec64
Erinç Uysal <erincuysal@outlook.com>
2023-07-03 00:51:36 +0300

    Inventory System (WIP)

--- Assets/Scripts/Core/Item.cs
+++ Assets/Scripts/Core/Item.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 namespace RPG.Core
 {
     public class Item : MonoBehaviour{
+        public ItemData itemData = new ItemData();
 
+    }
+    [System.Serializable]
+    public class ItemData{
+        public Sprite UI_Sprite;
+        public int amount = 1;
+        public bool canStack = true;
+        
+        public ItemData(){
+
+        }
     }
 
 }
--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using RPG.Core;
 
 namespace RPG.UI{
     public class InventoryUI : MonoBehaviour{
+        [SerializeField] Inventory inventory;
+        [SerializeField] private Transform itemSlotContainer;
+        [SerializeField] private Transform itemSlotTemplate;
+        private void Start() {
+            RefreshInventoryItems();
+        }
+        private void RefreshInventoryItems(){
+            int x =0;
+            int y = 0;
+            int xOfset = -54;
+            int yOfset = 19;
+            float itemSlotCellSize = 36f;
+            foreach(Item item in inventory.itemList){
+                RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+                itemSlotRectTransform.gameObject.SetActive(true);
+                itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize + xOfset, y*itemSlotCellSize + yOfset);
+                Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
+                image.sprite = item.itemData.UI_Sprite;
+                x++;
+                if (x >= 4){
+                    x= 0;
+                    y--;
+                }
+
+
+            }
+        } 
        
     }
 }
